671079bf2e7e9fb73307746a208ce1e336725eb8
EStothePO <134443335+EStothePO@users.noreply.github.com>
2023-05-23 18:14:01 -0400

    Update About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -39,19 +39,40 @@ const About = () => {
   return (
     <>
       <motion.div variants={textVariant()}>
-        <p className={styles.sectionSubText}>Introduction</p>
-        <h2 className={styles.sectionHeadText}>Overview.</h2>
+        <p className={styles.sectionSubText}>Biography</p>
+        <h2 className={styles.sectionHeadText}>Who I am.</h2>
       </motion.div>
 
       <motion.p
         variants={fadeIn("", "", 0.1, 1)}
         className='mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]'
       >
-        I'm a skilled software developer with experience in TypeScript and
-        JavaScript, and expertise in frameworks like React, Node.js, and
-        Three.js. I'm a quick learner and collaborate closely with clients to
-        create efficient, scalable, and user-friendly solutions that solve
-        real-world problems. Let's work together to bring your ideas to life!
+       On my web portfolio, you'll see the story of my journey from a high-school graduate to a multifaceted designer.
+       After completing high school, I embarked on a career as an automotive technician, where I honed my skills in vehicle repair and maintenance.
+       As time went on, I transitioned from being a mechanic to a CNC machinist, delving into the world of precision manufacturing. During my tenure 
+       as a machinist, I was introduced to the captivating realm of product design. This exposure sparked a passion within me for creating 
+       innovative, functional, and beautiful products. Driven by this newfound love, I made the bold decision to leave the workforce and return to academia,
+       enrolling in a university to study Industrial Design.
+       
+       As an Industrial Design student, I have acquired a diverse set of software and creative skills that have significantly enhanced my design capabilities.
+       Throughout my academic journey, I have become proficient in various essential tools and techniques that are vital in the world of design, 
+       Including but not limited to:
+       
+       BluePrint Drafting and reading 
+       AutoCAD
+       Fusion 360
+       Solidworks
+       Keyshot
+       Adobe Photoshop, Illustrator, various other Adobe Suite softwares
+       ________ WEB DEVELOPING SKILLS SHOULD GO HERE
+
+       These software and creative skills, acquired during my time as an Industrial Design student, have equipped me with a comprehensive toolkit to tackle 
+       a wide range of design challenges and contribute meaningfully to the field of product design and development.
+       Throughout my educational journey, I was able to seamlessly blend my technical expertise from my previous roles with my newly acquired creative abilities. 
+       This unique combination of skills has allowed me to evolve into the versatile designer I am today. Now, I am eager to apply my diverse skill set to tackle
+       new challenges and create meaningful, impactful designs that resonate with people and improve their everyday lives. 
+       I'm a quick learner and collaborate closely with clients to create efficient, scalable, and user-friendly solutions that solve real-world problems.
+       Let's work together to bring your ideas to life!
       </motion.p>
 
       <div className='mt-20 flex flex-wrap gap-10'>
